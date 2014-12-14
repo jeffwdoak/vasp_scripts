b2c0085b5c12d994cbd903a18cea889768532ac9
jeff.w.doak <jeff.w.doak@gmail.com>
2014-12-14 17:05:13 -0600

    Added comments to procar_awk.sh code.

--- vasp_scripts/procar_awk.sh
+++ vasp_scripts/procar_awk.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+
+# procar_awk.sh Jeff Doak jeff.w.doak@gmail.com
+
+# Awk script to extract the electron eigenvalues at each k-point of a VASP
+# calculation which have a specific projection weight onto an atomic oribtal.
+
+# The first commandline argument is the PROCAR file to analyze.
+# The second argument is the number of the ion in the POSCAR to look for 
+# projection onto.
+# The third and fourth arguments are the lower and upper bounds on the
+# projection weight, respectively.
+#
+# At each k-point, the bands which have a projection onto the atomic orbital of
+# the ion $ion with weight in between the lower and upper bounds will be
+# printed to standard out. This is useful for parsing the PROCAR file for bands
+# which project onto, say, an impurity atom, without also getting numerical
+# noise associated with the projection scheme.
+#
+# Example usage: procar_awk.sh PROCAR 1 0.01 0.3
+
 procar=$1
 ion=$2
 tol1=$3
